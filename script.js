c63ebc48b7918abefbfcade821ae49e4dd27a608
Tasheem <thargro1@uab.edu>
2022-10-23 09:53:13 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+document.getElementById('add-btn').addEventListener('click',
+(event) => {
+    const userInputs = getInputs(event.target);
+    const result = add(userInputs[0], userInputs[1]);
+
+    const parent = event.target.parentElement;
+    const outputDiv = parent.children[0];
+    outputDiv.textContent = result;
+});
+
+document.getElementById('subtract-btn').addEventListener('click',
+(event) => {
+    const userInputs = getInputs(event.target);
+    const result = subtract(userInputs[0], userInputs[1]);
+    
+    const parent = event.target.parentElement;
+    const outputDiv = parent.children[0];
+    outputDiv.textContent = result;
+})
+
+const add = (num1, num2) => {
+    if(typeof num1 !== 'number' || typeof num2 !== 'number') {
+        console.log('Two numbers must be entered into the add() function.');
+        return NaN;
+    }
+
+    const result = num1 + num2;
+    console.log(result);
+    return result;
+}
+
+const subtract = (num1, num2) => {
+    if(typeof num1 !== 'number' || typeof num2 !== 'number') {
+        console.log('Two numbers must be entered into the add() function.');
+        return NaN;
+    }
+
+    const result = num1 - num2;
+    console.log(result);
+    return result;
+}
+
+const getInputs = (element) => {
+    const parent = element.parentElement;
+    const inputContainer = parent.children[1];
+    const num1 = Number(inputContainer.children[0].value);
+    const num2 = Number(inputContainer.children[1].value);
+
+    return [num1, num2];
+}
