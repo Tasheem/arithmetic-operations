b01c698c21a1bb3b7d8e673a0bf074561392131d
Tasheem <thargro1@uab.edu>
2022-10-23 10:47:43 -0500

    Make functions more flexible

--- script.js
+++ script.js
@@ -1,54 +1,81 @@
 const add = (num1, num2) => {
+    num1 = Number(num1);
+    num2 = Number(num2);
     if(!isValidType(num1, num2)) {
-        console.log('Two numbers must be entered into the add() function.');
+        console.error('Two numbers must be entered into the add() function.');
         return NaN;
     }
 
     const result = num1 + num2;
-    console.log(result);
+    console.log(`Add: ${result}`);
     return result;
 }
 
 const subtract = (num1, num2) => {
     if(!isValidType(num1, num2)) {
-        console.log('Two numbers must be entered into the subtract() function.');
+        console.error('Two numbers must be entered into the subtract() function.');
         return NaN;
     }
 
     const result = num1 - num2;
-    console.log(result);
+    console.log(`Subtract: ${result}`);
     return result;
 }
 
-const isValidType = (num1, num2) => {
-    return typeof num1 === 'number' && typeof num2 === 'number';
+const multiply = (num1, num2) => {
+    if(!isValidType(num1, num2)) {
+        console.error('Two numbers must be entered into the multiply() function.');
+        return NaN;
+    }
+
+    const result = num1 * num2;
+    console.log(`Multiply: ${result}`);
+    return result;
 }
 
-document.getElementById('add-btn').addEventListener('click',
-(event) => {
-    const userInputs = getInputs(event.target);
-    const result = add(userInputs[0], userInputs[1]);
+const combo = (num1, num2) => {
     
-    const parent = event.target.parentElement;
-    const outputDiv = parent.children[0];
-    outputDiv.textContent = result;
+}
+
+const isValidType = (num1, num2) => {
+    return !isNaN(num1) && !isNaN(num2);
+}
+
+// Display output of operation in the card.
+const displayResult = (event) => {
+    const button = event.target;
+    const userInputs = getInputs(button);
+
+    let result;
+    if(button.id === 'add-btn')
+        result = add(userInputs[0], userInputs[1]);
+    else if(button.id === 'subtract-btn')
+        result = subtract(userInputs[0], userInputs[1]);
+    else if(button.id === 'multiply-btn')
+        result = multiply(userInputs[0], userInputs[1]);
+    
+    const card = button.parentElement;
+    const outputDiv = card.children[0];
+
+    if(isNaN(result))
+        outputDiv.textContent = '';
+    else
+        outputDiv.textContent = result;
+}
+
+// Add event listener to each button on the page.
+// displayResult() is passed as the callback.
+const runButtons = document.querySelectorAll('.run-btn');
+runButtons.forEach(btn => {
+    btn.addEventListener('click', displayResult);
 });
 
-document.getElementById('subtract-btn').addEventListener('click',
-(event) => {
-    const userInputs = getInputs(event.target);
-    const result = subtract(userInputs[0], userInputs[1]);
-    
-    const parent = event.target.parentElement;
-    const outputDiv = parent.children[0];
-    outputDiv.textContent = result;
-});
-
-const getInputs = (element) => {
-    const parent = element.parentElement;
-    const inputContainer = parent.children[1];
-    const num1 = Number(inputContainer.children[0].value);
-    const num2 = Number(inputContainer.children[1].value);
+// Get user input for specific fields associated with the clicked button/card.
+const getInputs = (button) => {
+    const card = button.parentElement;
+    const inputContainer = card.children[1];
+    const num1 = inputContainer.children[0].value;
+    const num2 = inputContainer.children[1].value;
 
     return [num1, num2];
 }
